18aa62c2c872b2e978222926799e907de1a8add4
Hamsini Sankaran <hamsini0992@gmail.com>
2018-05-16 12:02:05 -0700

    Firstgame

---
+++ myscript.js
@@ -0,0 +1,31 @@
+
+//Restart the game button
+var restart = document.querySelector("#b")
+
+//grab all the squares
+var squares = document.querySelectorAll('td');
+
+//clear all the squares
+function clearBoard(){
+  for(var i = 0; i < squares.length; i++){
+    squares[i].textContent = '';
+  }
+}
+
+restart.addEventListener('click',clearBoard);
+
+//check the square number
+function changeMarker(){
+if(this.textContent === ''){
+  this.textContent = 'X';
+}else if(this.textContent === 'X'){
+  this.textContent = 'O'
+}else{
+  this.textContent = ' ';
+}
+}
+
+
+for(var i=0; i < squares.length; i++){
+  squares[i].addEventListener('click', changeMarker)
+}
